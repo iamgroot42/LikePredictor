4aac386b47f0ca78e6180cf3f72075ad847b151a
that-1guy <satyam14096@iiitd.ac.in>
2015-11-14 17:11:42 +0530

    Formatted Final Page

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -19,14 +19,14 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/ResultPage")
 public class ResultPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ResultPage() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ResultPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,16 +37,43 @@ public class ResultPage extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		HttpSession session=request.getSession();
 		Result ret=(Result)session.getAttribute("results");
-//		session.invalidate();
-		writer.println("<html>");
+		//		session.invalidate();
+		writer.println("<html lang='en'>");
 		writer.println("<head>");
-		writer.println("<link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">");
-		writer.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>");
-		writer.println("<script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>");
+		writer.println("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>");
+		writer.println("<meta charset='utf-8'>");
+		writer.println("<title>Like Predictor : Results</title>");
+		writer.println("<meta name='generator' content='Bootply' />");
+		writer.println("<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>");
+		writer.println("<link href='css/bootstrap.min.css' rel='stylesheet'>");
+		writer.println("<link href='css/styles.css' rel='stylesheet'>");
+		writer.println("</head>");
+		
+		
 		writer.println("<body>");
+		
+		writer.println("<div class='padding'>");
+			writer.println("<div class='row well'>");
+				writer.println("<div class='col-sm-12'>");
+					writer.println("<div class='panel panel-default'>");
+						writer.println("<div class='panel-thumbnail'></div>");
+							writer.println("<div class='panel-body'>");
+								writer.println("<h class='lead'>");
+									writer.println("<center>");
+										writer.println(" Results!");
+									writer.println(" </center>");
+								writer.println("</h>");
+								writer.println("<p><center>");
+									writer.println("<img src='https://lh3.googleusercontent.com/uFp_tsTJboUY7kue5XAsGA=s28' width='28px' height='28px'>");
+								writer.println("</center></p>");
+							writer.println("</div>");
+					writer.println("</div>");
+				writer.println("</div>");
+			writer.println("</div>");
+		writer.println("</div>");
 		if(ret!=null)
 		{
-//			writer.println("Training error : "+ret.getPercentage_error()+"%");
+			//			writer.println("Training error : "+ret.getPercentage_error()+"%");
 			HashMap<String,Long> mapping=WhoWillLike.getMapping();
 			HashMap<String,String> iterate=ret.getLikers();
 			long tot=WhoWillLike.getTotal();
@@ -88,31 +115,54 @@ public class ResultPage extends HttpServlet {
 			writer.println("<br>");
 			ArrayList<Long> actual=ret.getActual_likes();
 			ArrayList<Long> predicted=ret.getPredicted_likes();
+			writer.println("<div class='column col-sm-12 col-xs-11' id='main'>");
+			writer.println("<div class='padding'>");
+			writer.println("<div class='row well'>");
 			int j = 0, k;
 			long end;
 			for(String x:ret.getPost_links())
 			{
-				writer.println("<br>");
-				writer.println("<a href=\""+x+"\">Click here to view post</a>");
-				writer.println("Actual Likes : " + actual.get(j) /*+ "; Predicted Likes : " + predicted.get(j)*/);
-				writer.print("Likers : ");
-				//end = predicted.get(j); k = 0;
-				end = actual.get(j); k = 0;
-				for(String str : mapping.keySet()) {
-					if(k++ == actual.get(j)) break;
-					writer.print(iterate.get(str));
-					if(k != end) writer.print(", ");
-				}
-				writer.println("<br>");
-				writer.println("<br>");
-				j++;
+				writer.println("<div class='col-sm-6'>");
+					writer.println("<div class='panel panel-default'>");
+						writer.println("<div class='panel-heading'>");
+							writer.println("<h4>");
+								writer.println("<a href='" + x + "'>Link to Post</a>");
+							writer.println("</h4>");
+						writer.println("</div>");
+						writer.println("<div class='panel-body'>");
+							writer.println("<div class='list-group'>");
+								writer.println("<p class='list-group-item'>Number of Predicted Likes :</p>");
+								writer.println("<p class='list-group-item'>Number of Actual Likes :</p>");
+							writer.println("</div>");
+						writer.println("</div>");
+					writer.println("</div>");
+				writer.println("</div>");
+				//writer.println("<a href=\""+x+"\">Click here to view post</a>");
+//				writer.println("Actual Likes : " + actual.get(j) /*+ "; Predicted Likes : " + predicted.get(j)*/);
+//				writer.print("Likers : ");
+//				//end = predicted.get(j); k = 0;
+//				end = actual.get(j); k = 0;
+//				for(String str : mapping.keySet()) {
+//					if(k++ == actual.get(j)) break;
+//					writer.print(iterate.get(str));
+//					if(k != end) writer.print(", ");
+//				}
+//				writer.println("<br>");
+//				writer.println("<br>");
+//				j++;
 			}
-//			writer.println("Actual"+"          "+"Predicted");
-//			for(int i=0;i<actual.size();++i)
-//			{
-//				writer.println(actual.get(i)+"          "+predicted.get(i));
-//			}
+			//			writer.println("Actual"+"          "+"Predicted");
+			//			for(int i=0;i<actual.size();++i)
+			//			{
+			//				writer.println(actual.get(i)+"          "+predicted.get(i));
+			//			}
 		}
+		writer.println("</div>");
+		writer.println("</div>");
+		writer.println("</div>");
+		writer.println("<script src='//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js'></script>");
+		writer.println("<script src='js/bootstrap.min.js'></script>");
+		writer.println("<script src='js/scripts.js'></script>");
 		writer.println("</body>");
 		writer.println("</html>");
 	}
