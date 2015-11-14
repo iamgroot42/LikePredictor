14710d156bac93ae96caf0540c3a9907dfacf157
Anshuman <anshuman14021@iiitd.ac.in>
2015-11-14 18:30:09 +0530

    Added piechart (Google Charts). Removed progress bars.

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -37,43 +37,23 @@ public class ResultPage extends HttpServlet {
 		PrintWriter writer = response.getWriter();
 		HttpSession session=request.getSession();
 		Result ret=(Result)session.getAttribute("results");
-		//		session.invalidate();
-		writer.println("<html lang='en'>");
-		writer.println("<head>");
-		writer.println("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>");
-		writer.println("<meta charset='utf-8'>");
-		writer.println("<title>Like Predictor : Results</title>");
-		writer.println("<meta name='generator' content='Bootply' />");
-		writer.println("<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>");
-		writer.println("<link href='css/bootstrap.min.css' rel='stylesheet'>");
-		writer.println("<link href='css/styles.css' rel='stylesheet'>");
-		writer.println("</head>");
-		
-		
-		writer.println("<body>");
-		
-		writer.println("<div class='padding'>");
-			writer.println("<div class='row well'>");
-				writer.println("<div class='col-sm-12'>");
-					writer.println("<div class='panel panel-default'>");
-						writer.println("<div class='panel-thumbnail'></div>");
-							writer.println("<div class='panel-body'>");
-								writer.println("<h class='lead'>");
-									writer.println("<center>");
-										writer.println(" Results!");
-									writer.println(" </center>");
-								writer.println("</h>");
-								writer.println("<p><center>");
-									writer.println("<img src='https://lh3.googleusercontent.com/uFp_tsTJboUY7kue5XAsGA=s28' width='28px' height='28px'>");
-								writer.println("</center></p>");
-							writer.println("</div>");
-					writer.println("</div>");
-				writer.println("</div>");
-			writer.println("</div>");
-		writer.println("</div>");
-		if(ret!=null)
+		try
 		{
-			//			writer.println("Training error : "+ret.getPercentage_error()+"%");
+			//		session.invalidate();
+			writer.println("<html lang='en'>");
+			writer.println("<head>");
+			writer.println("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>");
+			writer.println("<meta charset='utf-8'>");
+			writer.println("<title>Like Predictor : Results</title>");
+			writer.print("<script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>");
+			writer.print("<script type=\"text/javascript\">");
+			writer.print("google.load('visualization', '1.0', {'packages':['corechart']});");
+			writer.print("google.setOnLoadCallback(drawChart);");
+			writer.print("function drawChart() {");
+			writer.print("var data = new google.visualization.DataTable();");
+			writer.print("data.addColumn('string', 'Name');");
+			writer.print("data.addColumn('number', 'Likes');");
+			writer.print("data.addRows([");
 			HashMap<String,Long> mapping=WhoWillLike.getMapping();
 			HashMap<String,String> iterate=ret.getLikers();
 			long tot=WhoWillLike.getTotal();
@@ -81,42 +61,61 @@ public class ResultPage extends HttpServlet {
 			String type;
 			for(String iterator : iterate.keySet())
 			{
-				if(i%4==0)
-				{
-					type="success";
-				}
-				else if(i%4==1)
-				{
-					type="info";
-				}
-				else if(i%4==2)
-				{
-					type="warning";
-				}
-				else
-				{
-					type="danger";
-				}
-				writer.println("<div class='container' style='margin-left:0%'>");
-				writer.println("<div class=\"progress\" style='margin-bottom:0.2%'>");
-				double yoda=(((double)mapping.get(iterator))/(double)tot)*(100.0);
-				writer.println("<div class=\"progress-bar progress-bar-striped progress-bar-"+type+"\" role=\"progressbar\" aria-valuenow=\""+yoda+"\"aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:"+yoda+"%\">");
-				writer.println("<span style='color:black; position:absolute; display:block; width: 100%;'>"); 
-				writer.println(iterate.get(iterator));
-				writer.println("</span>");
-				writer.println("</div>");
-				writer.println("</div>");
-				writer.println("</div>");
+				if(i>=10) break; //Top 10 only
+				writer.print("['");
+				writer.print(iterate.get(iterator));
+				writer.print("',");
+				writer.print(mapping.get(iterator));
+				writer.print("],");
 				i++;
+				tot-=mapping.get(iterator);
 			}
-			writer.println("<br>");
-			writer.println("<br>");
-			writer.println("Absolute training error : "+ret.getAbsolute_error());
-			writer.println("<br>");
-			writer.println("Percentage training error : "+ret.getPercentage_error());
-			writer.println("<br>");
+			if(tot>0)
+			{
+				writer.print("['Others',"+tot+"],");
+			}
+			writer.print("]);");
+			writer.print("var options = {'title':'Distrubution of likes',");
+			writer.print("'width':900,");
+			writer.print("'chartArea':{left:'10%',top:'20%',width:'90%',height:'75%'},");
+			writer.print("'height':625};");
+			writer.print("var chart = new google.visualization.PieChart(document.getElementById('chart_div'));");
+			writer.print("chart.draw(data, options);");
+			writer.print("} </script>");
+			writer.println("<meta name='generator' content='Bootply' />");
+			writer.println("<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>");
+			writer.println("<link href='css/bootstrap.min.css' rel='stylesheet'>");
+			writer.println("<link href='css/styles.css' rel='stylesheet'>");
+			writer.println("</head>");
+			writer.println("<body>");
+//			writer.println("<div class='padding'>");
+//			writer.println("<div class='row well'>");
+//				writer.println("<div class='col-sm-12'>");
+//					writer.println("<div class='panel panel-default'>");
+//							writer.println("<div class='panel-body'>");
+//								writer.println("<h class='lead'>");
+//									writer.println("<center>");
+//										writer.println(" Results!");
+//									writer.println(" </center>");
+//								writer.println("</h>");
+//								writer.println("<p><center>");
+//									writer.println("<img src='https://lh3.googleusercontent.com/uFp_tsTJboUY7kue5XAsGA=s28' width='28px' height='28px'>");
+//								writer.println("</center></p>");
+//							writer.println("</div>");
+//					writer.println("</div>");
+//				writer.println("</div>");
+//			writer.println("</div>");
+//		writer.println("</div>");
+//			writer.println("Absolute training error : "+ret.getAbsolute_error());
+//			writer.println("<br>");
+//			writer.println("Percentage training error : "+ret.getPercentage_error());
+//			writer.println("<br>");
 			ArrayList<Long> actual=ret.getActual_likes();
 			ArrayList<Long> predicted=ret.getPredicted_likes();
+			
+			//Render chart :
+			writer.println(" <div id=\"chart_div\"></div>");
+			
 			writer.println("<div class='column col-sm-12 col-xs-11' id='main'>");
 			writer.println("<div class='padding'>");
 			writer.println("<div class='row well'>");
@@ -133,8 +132,8 @@ public class ResultPage extends HttpServlet {
 						writer.println("</div>");
 						writer.println("<div class='panel-body'>");
 							writer.println("<div class='list-group'>");
-								writer.println("<p class='list-group-item'>Number of Predicted Likes : " + actual.get(j) +"</p>");
-								writer.println("<p class='list-group-item'>Number of Actual Likes : " + predicted.get(j) +"</p>");
+								writer.println("<p class='list-group-item'>Predicted Likes : " + predicted.get(j) +"</p>");
+								writer.println("<p class='list-group-item'>Actual Likes : " + actual.get(j) +"</p>");
 							writer.println("</div>");
 						writer.println("</div>");
 					writer.println("</div>");
@@ -142,7 +141,6 @@ public class ResultPage extends HttpServlet {
 //				writer.print("PredLikers : ");
 				j++;
 			}
-		}
 		writer.println("</div>");
 		writer.println("</div>");
 		writer.println("</div>");
@@ -151,6 +149,11 @@ public class ResultPage extends HttpServlet {
 		writer.println("<script src='js/scripts.js'></script>");
 		writer.println("</body>");
 		writer.println("</html>");
+		}
+		catch(Exception e)
+		{
+			writer.println("Fraud hua beta,fraud");
+		}
 	}
 
 	/**
