51ac0f050cafc9a9d6fc368bebb84a8ec32f0f3e
Anshuman <anshuman14021@iiitd.ac.in>
2015-11-14 22:23:53 +0530

    Added one more graph to result page.Looks awesome now ^_^.

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -39,12 +39,13 @@ public class ResultPage extends HttpServlet {
 		Result ret=(Result)session.getAttribute("results");
 		try
 		{
-			//		session.invalidate();
 			writer.println("<html lang='en'>");
 			writer.println("<head>");
 			writer.println("<meta http-equiv='content-type' content='text/html; charset=UTF-8'>");
 			writer.println("<meta charset='utf-8'>");
 			writer.println("<title>Like Predictor : Results</title>");
+			
+			//First pie chart :
 			writer.print("<script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>");
 			writer.print("<script type=\"text/javascript\">");
 			writer.print("google.load('visualization', '1.0', {'packages':['corechart']});");
@@ -56,6 +57,8 @@ public class ResultPage extends HttpServlet {
 			writer.print("data.addRows([");
 			HashMap<String,Long> mapping=WhoWillLike.getMapping();
 			HashMap<String,String> iterate=ret.getLikers();
+			ArrayList<Long> actual=ret.getActual_likes();
+			ArrayList<Long> predicted=ret.getPredicted_likes();
 			long tot=WhoWillLike.getTotal();
 			int i=0;
 			String type;
@@ -75,10 +78,7 @@ public class ResultPage extends HttpServlet {
 				writer.print("['Others',"+tot+"],");
 			}
 			writer.print("]);");
-			writer.print("var options = {'title':'Distrubution of likes',");
-			writer.print("'width':900,");
-			writer.print("'chartArea':{left:'10%',top:'20%',width:'90%',height:'75%'},");
-			writer.print("'height':625};");
+			writer.print("var options = {'title':'Distrubution of likes'};");
 			writer.print("var chart = new google.visualization.PieChart(document.getElementById('chart_div'));");
 			writer.print("chart.draw(data, options);");
 			writer.print("} </script>");
@@ -86,6 +86,35 @@ public class ResultPage extends HttpServlet {
 			writer.println("<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'>");
 			writer.println("<link href='css/bootstrap.min.css' rel='stylesheet'>");
 			writer.println("<link href='css/styles.css' rel='stylesheet'>");
+			writer.println("<link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">");
+			writer.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>");
+			writer.println("<script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>");
+			
+
+			//Second line chart :
+			writer.print(" <script type=\"text/javascript\"src=\"https://www.google.com/jsapi?autoload={ 'modules':[{ 'name':'visualization','version':'1','packages':['corechart']}]}\"></script>");
+			writer.print("<script type=\"text/javascript\">");
+			writer.print("google.setOnLoadCallback(drawChart);");
+			writer.print("function drawChart() {");
+			writer.print("var data = google.visualization.arrayToDataTable([");
+			writer.print(" ['Number', 'Predicted', 'Actual'],");
+			int nono,hehe;
+			nono=predicted.size();
+			for(hehe=0;hehe<nono;hehe++)
+			{
+				writer.print("['"+(hehe+1)+"',");
+				writer.print(predicted.get(hehe)+",");
+				writer.print(actual.get(hehe)+"],");
+			}
+			writer.print("]);");
+			writer.print("var options = {");
+			writer.print("title: 'Actual v/s Prediction',");
+			writer.print("curveType: 'function',");
+			writer.print("legend: { position: 'bottom' }");
+			writer.print("};");
+			writer.print("var chart = new google.visualization.LineChart(document.getElementById('potato'));");
+			writer.print(" chart.draw(data, options); }");
+			writer.print(" </script>");
 			writer.println("</head>");
 			writer.println("<body>");
 			//			writer.println("<div class='padding'>");
@@ -110,12 +139,14 @@ public class ResultPage extends HttpServlet {
 			//			writer.println("<br>");
 			//			writer.println("Percentage training error : "+ret.getPercentage_error());
 			//			writer.println("<br>");
-			ArrayList<Long> actual=ret.getActual_likes();
-			ArrayList<Long> predicted=ret.getPredicted_likes();
 
 			//Render chart :
-			writer.println(" <div id=\"chart_div\"></div>");
-
+			writer.println("<div class=\"row\">");
+//			writer.println("")
+			writer.println("<div style=\"width:50%; height:50%;\" class=\"col-sm-6\" id=\"chart_div\"></div>");
+			writer.println("<div style=\"width:50%; height:50%;\" class=\"col-sm-6\" id=\"potato\"></div>");
+			writer.println("</div>");			
+			
 			writer.println("<div class='column col-sm-12 col-xs-11' id='main'>");
 			writer.println("<div class='padding'>");
 			writer.println("<div class='row well'>");
@@ -129,7 +160,7 @@ public class ResultPage extends HttpServlet {
 				writer.println("<form action='" + x + "'>");
 				writer.println("<button class='btn btn-primary pull-right' type='submit'>&nbsp; View Post &nbsp</button>");
 				writer.println("</form>");    
-				if(-5 < predicted.get(j)-actual.get(j) & predicted.get(j)-actual.get(j)<5)
+				if(-5 <= predicted.get(j)-actual.get(j) & predicted.get(j)-actual.get(j)<=5)
 				{
 					writer.println("<h4 style=\"color:green\">");
 					writer.println("Correct prediction");
@@ -149,7 +180,7 @@ public class ResultPage extends HttpServlet {
 				writer.println("</div>");
 				writer.println("</div>");
 				writer.println("</div>");
-				//				writer.print("PredLikers : ");
+				//writer.print("PredLikers : ");
 				j++;
 			}
 			writer.println("</div>");
